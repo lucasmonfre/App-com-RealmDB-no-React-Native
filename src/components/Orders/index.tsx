4f0cc7dc5e1dccf2e832d1784100377cc3fcabf2
lucas monfre <lucas.monfre@hotmail.com>
2022-08-18 15:38:54 -0300

    primeiro commit

---
+++ src/components/Orders/index.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useCallback } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { getRealm } from '../../database/realm';
+
+
+import { Load } from '../Load';
+import { Filters } from '../Filters';
+import { Order, OrderProps } from '../Order';
+
+import { Container, Header, Title, Counter } from './styles';
+
+export function Orders() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+  const [status, setStatus] = useState('open');
+
+  async function fetchOrders() {
+    setIsLoading(true);
+    const realm = await getRealm();
+
+    try {
+      const response = realm
+      .objects<OrderProps[]>("Order")
+      .filtered(`status = '${status}'`)
+      .sorted("created_at")
+      .toJSON();
+
+      setOrders(response);
+
+    } catch {
+      Alert.alert("Chamados", "Não foi possível carregar os chamados");
+    } finally {
+      realm.close();
+      setIsLoading(false);
+    }
+  }
+
+  async function OrderUpdate(id: string){
+    const realm = await getRealm();
+
+    try {
+      const orderSelected = realm
+      .objects<OrderProps>('Order')
+      .filtered(`_id = '${id}'`)[0]
+
+      realm.write(() => {
+        orderSelected.status = orderSelected.status === "open" ? "closed" : "open";
+      });
+
+      Alert.alert("Chmado", "Chamado atualizado!");
+      fetchOrders();
+      
+    } catch {
+      Alert.alert("Chamados", "Não foi possível atualizar os chamados");
+    }
+  }
+
+  function handleOrderUpadate(id: string) {
+    Alert.alert(
+    "Chamado",
+    "Encerrar chamado?",
+    [
+      {
+        text: "Cancelar",
+        style: "cancel"
+      },
+      {
+        text: "Confirmar",
+        onPress: () => OrderUpdate(id)
+      }
+    ]
+    )
+  }
+
+  useFocusEffect(useCallback(() => {
+    fetchOrders();
+  },[status]));
+
+  return (
+    <Container>
+      <Filters onFilter={setStatus} />
+
+      <Header>
+        <Title>Chamados {status === 'open' ? 'aberto' : 'encerrado'}</Title>
+        <Counter>{orders.length}</Counter>
+      </Header>
+
+      {
+        isLoading ?
+          <Load />
+          : <FlatList
+            data={orders}
+            keyExtractor={item => item._id}
+            renderItem={({ item }) => (
+              <Order data={item} onPress={() => handleOrderUpadate(item._id)} />
+            )}
+            contentContainerStyle={{ paddingBottom: 100 }}
+            showsVerticalScrollIndicator={false}
+            style={{ flex: 1 }}
+          />
+      }
+    </Container>
+  );
+}
---
+++ src/screens/NewOrder/index.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import uuid from 'react-native-uuid';
+import { getRealm } from '../../database/realm';
+
+import { Container, Header, Title, Form } from './styles';
+
+import { Input } from '../../components/Input';
+import { Button } from '../../components/Button';
+import { TextArea } from '../../components/TextArea';
+import { IconButton } from '../../components/IconButton';
+import { Alert } from 'react-native';
+
+export function NewOrder() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState('');
+  const [equipment, setEquipment] = useState('');
+  const [description, setDescription] = useState('');
+
+  const navigation = useNavigation();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
+  async function handleNewOrderRegister(){
+    const realm = await getRealm();
+
+    try {
+      setIsLoading(true);
+
+        realm.write(() => {
+        const created = realm.create('Order', {
+          _id: uuid.v4(),
+          patrimony,
+          equipment,
+          description,
+          status:'open',
+          created_at: new Date(),
+        });
+       });
+
+    
+      Alert.alert("Chamado", "Chamado cadastrado com sucesso!");
+    } catch {
+      Alert.alert("Chamado", "Não foi possivel abrir o chamado!");
+    } finally {
+      (await realm).close();
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Title>Novo chamado</Title>
+        <IconButton icon="chevron-left" onPress={handleBack} />
+      </Header>
+
+      <Form>
+        <Input
+          placeholder="Número do Patrimônio"
+          onChangeText={setPatrimony}
+        />
+
+        <Input
+          placeholder="Equipamento"
+          onChangeText={setEquipment}
+        />
+
+        <TextArea
+          placeholder="Descrição"
+          autoCorrect={false}
+          onChangeText={setDescription}
+        />
+      </Form>
+
+      <Button
+        title="Enviar chamado"
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
+      />
+    </Container>
+  );
+}
